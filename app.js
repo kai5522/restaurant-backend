41f932308218e19de7ade0e74c2a9c02cd362b02
kai5522 <daspriyanshu48@gmail.com>
2024-09-05 21:22:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config({ path:"./config/config.env"});
 
 app.use(
   cors({
-    origin:'https://restaurant-frontend-bu5s.vercel.app',
+    origin:https://restaurant-frontend-dun.vercel.app/',
     methods: ["POST"],
     credentials: true,
   })
